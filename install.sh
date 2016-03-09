fbab63b931bf67b91f377bf2f3b0d218ca552e35
Fabian Grutschus <f.grutschus@lubyte.de>
2016-03-09 11:47:48 +0100

    Install to gits standard path

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-source_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# create symlink, otherwise git won't find commands
-ln -s "$source_dir" "$HOME/.my-git"
-git config --global include.path "~/.my-git/gitconfig"
-git config --global core.excludesFile "~/.my-git/global-gitignore"
